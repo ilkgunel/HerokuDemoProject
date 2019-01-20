9404bab33dbca3fe7b6b165079b097aabe67326f
İlkay Günel <ilkgunel93@gmail.com>
2019-01-20 10:10:24 +0300

    Initial commit

---
+++ src/main/java/com/ilkaygunel/pojo/FormulaOneDriver.java
@@ -0,0 +1,33 @@
+package com.ilkaygunel.pojo;
+
+public class FormulaOneDriver {
+
+	public FormulaOneDriver() {
+
+	}
+
+	public FormulaOneDriver(String name, String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+
+	private String name;
+	private String surname;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+}
